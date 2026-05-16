67190df30887e985666c69e460721226c2445fd5
Devon Cash <devon.p.cash@gmail.com>
2026-05-15 21:24:04 -0700

    Vinyl disc label: band name in top arc, CMC in bottom arc
    Swap arc text so each band's name appears on the top arc and
    "Corvallis Music Collective" appears on the bottom. Reduce bottom
    arc font-size and letter-spacing to fit the longer text.

--- src/routes/(public)/directory/+page.svelte
+++ src/routes/(public)/directory/+page.svelte
@@ -100,10 +100,10 @@
 												<path id="arc-bot-{b.id}" d="M 85,50 a 35,35 0 1,1 -70,0" fill="none" />
 											</defs>
 											<text>
-												<textPath href="#arc-top-{b.id}" startOffset="50%" text-anchor="middle">Corvallis Music</textPath>
+												<textPath href="#arc-top-{b.id}" startOffset="50%" text-anchor="middle">{b.name}</textPath>
 											</text>
 											<text>
-												<textPath href="#arc-bot-{b.id}" startOffset="50%" text-anchor="middle">Collective</textPath>
+												<textPath href="#arc-bot-{b.id}" startOffset="50%" text-anchor="middle">Corvallis Music Collective</textPath>
 											</text>
 										</svg>
 										<img class="vinyl-card__logo" src={speakerLogo} alt="" />
